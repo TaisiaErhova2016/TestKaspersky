87d0ebf1791158e7fe7510ab9d4e10b9eb5f535c
TaisiaErhova2016 <e.t.k.work.2016@yandex.ru>
2016-06-21 09:48:03 +0300

    Задачи

---
+++ TestKasperskyTask1/TestKasperskyTask1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TestKasperskyTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Начало работы");
+            ServiceMessage<Int32> serviceMessage = ServiceMessage<Int32>.Instance;
+            Thread thread = new Thread(() => Console.WriteLine("Выполнили сообщение:" + serviceMessage.Pop().ToString()));
+            thread.IsBackground = true;
+            thread.Start();
+            serviceMessage.Push(32);
+            Console.ReadLine();
+        }
+
+        public sealed class ServiceMessage<T>
+        {
+            private readonly ReaderWriterLockSlim m_lock = new ReaderWriterLockSlim();
+            private readonly Queue<T> msgQueue = new Queue<T>();
+            //Обработчик сообщений
+            public T Pop()
+            {
+                Console.WriteLine("Мы тут");
+                Object message = new Object();
+                m_lock.EnterReadLock();
+                if (this.msgQueue.Count > 0)
+                    message = this.msgQueue.Dequeue();
+                m_lock.ExitReadLock();
+                return (T)message;
+            }
+            // Добавление сообщений в очередь
+            public void Push(T message)
+            {
+                m_lock.EnterWriteLock();
+                Thread.Sleep(10000); //для тестирования
+                this.msgQueue.Enqueue(message);
+                Console.WriteLine("Поставили сообщение в очередь: " + message.ToString());
+                m_lock.ExitWriteLock();
+            }
+
+            //Singleton для объявления очереди
+            private static volatile ServiceMessage<T> instance;
+            private static object sync_ = new Object();
+            private ServiceMessage() { }
+
+            public static ServiceMessage<T> Instance
+            {
+                get
+                {
+                    if (instance == null)
+                    {
+                        lock (sync_)
+                        {
+                            if (instance == null)
+                                instance = new ServiceMessage<T>();
+                        }
+                    }
+                    return instance;
+                }
+            }
+        }
+    }
+}
---
+++ TestKasperskyTask2/TestKaspersky/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestKaspersky
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 2. Первый вариант решения
+            Int32 X = 10;
+            var numbers = new Int32[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            //Циклом
+            Array.Sort(numbers);
+            int first = 0;
+            int last = numbers.Count() - 1;
+            while (first < last)
+            {
+                int s = numbers[first] + numbers[last];
+                if (s == X)
+                {
+                    Console.WriteLine(numbers[first] + " " + numbers[last]);
+                    first++;
+                    last--;
+                }
+                else
+                {
+                    if (s < X) first++;
+                    else last--;
+                }
+            }
+            Console.ReadLine();
+
+            //Linq to Object
+            var numbersLinq =
+                (from Item in numbers
+                 join Item2 in numbers
+                 on numbers[Array.IndexOf(numbers, Item)] equals (X - numbers[Array.IndexOf(numbers, Item2)])
+                 where Array.IndexOf(numbers, Item) != Array.IndexOf(numbers, Item2)
+                        &&
+                        numbers[Array.IndexOf(numbers, Item)] <= numbers[Array.IndexOf(numbers, Item2)]
+                 select new
+                 {
+                     FirstNumber = numbers[Array.IndexOf(numbers, Item)],
+                     SecondNumber = numbers[Array.IndexOf(numbers, Item2)]
+                 }
+                 );
+
+            foreach (var n in numbersLinq)
+            {
+                Console.WriteLine("{0} + {1} = {2}", n.FirstNumber, n.SecondNumber, X);
+            }
+            Console.ReadLine();
+        }
+    }
+
+        
+}
